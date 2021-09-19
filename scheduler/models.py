6f67520ebc40709646d892fafb671f9854c59aad
brasmussen117 <brenden.rasmussen.117@gmail.com>
2021-09-19 11:13:30 -0400

    clean up
    improved gitignore
    
    remove .pyc files
    
    merge

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
 # Create your models here.
+class Semester(models.Model):
+
+    # All of the items that create a semester
+    crn = models.IntegerField(unique = True)
+    dept = models.CharField(max_length= 4)
+    course_id = models.CharField(max_length=9)
+    section = models.IntegerField()
+    status = models.CharField(max_length= 1)
+    title = models.TextField()
+    link1 = models.CharField(max_length=2)
+    link2 = models.CharField(max_length=2)
+    schedule_type = models.CharField(max_length=2)
+    reserved = models.CharField(max_length=1)
+    credit_hours = models.IntegerField()
+    billing_hours = models.IntegerField()
+    contact_hours = models.IntegerField()
+    grad_able = models.CharField(max_length=1)
+    cap = models.IntegerField()
+    waitlist_cap = models.IntegerField()
+    spec_appr = models.CharField(max_length=5)
+    meeting_type = models.CharField(max_length= 5)
+    begin_date = models.DateField()
+    end_date = models.DateField()
+    days = models.CharField(max_length=8)                               # Check to make sure time and days exist in meeting_time
+    time = models.IntegerField()                                        # Check for valid time in meeting_time
+    location = models.IntegerField()                                    # Check for valid location in location table
+    site_code = models.CharField(max_length=5)
+    primary_instructor = models.IntegerField()                          # Check for valid teacher in teacher table
+    fee = models.IntegerField()
+    comment = models.TextField()
+
+# Meeting times table
+class Meeting_Times(models.Model):
+
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    days = models.CharField(max_length=8)
+
+# Rooms table
+class Rooms(models.Model):
+    #department =  models.TextField()
+    campus = models.TextField()
+    building = models.TextField()
+    room_num = models.TextField()
+    capacity = models.IntegerField()
+    room_type = models.CharField(max_length=3)
+
+# Instructors table
+class Instructors(models.Model):
+    last_name = models.TextField()
+    first_name = models.TextField()
+    status = models.CharField(max_length=2)
+    department = models.CharField(max_length=4)
+    schedule = models.JSONField()
+
+def fields(self):
+    return [ f.name for f in self._meta.fields + self._meta.many_to_many ]
+
+
+'''
+class Column_Names (modes.Model):
+    page_name = models.TextField()
+    column_name = models.TextField()
+    modeified_date = models.DateField()
+
+
+'''
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,8 +1,54 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from scheduler.models import Instructors, Rooms
+from scheduler.models import fields
+
+coloumns =[
+    'class',
+    'room',
+    'location',
+    'credits',
+    'hours'
+]
+
+
+data2 = list(Instructors.objects.values_list())
+colums_info = fields(Instructors)
+
+data = [
+    {
+        'class':"Computer Science 1",
+        'room':"C100",
+        'time':"1400",
+        'location':"Parkview",
+        'credits':"3",
+        'hours': "3"
+    },
+    {
+        'class':"Databases",
+        'room':"D208",
+        'time':"1100",
+        'location':"Parkview",
+        'credits':"4",
+        'hours': "4"
+    }
+]
 
 # Create your views here.
 def main_page(request):
     context = {
+        'input': data2,
+        'col': colums_info
+    }
+    return render(request, 'scheduler/home.html', context)
 
+def room_page(request):
+    context = {
+        #'input': Rooms.objects.filter(
+            #first_name__startswith='R'
+            #).values('first_name', 'last_name')
+        'input': Rooms.objects.all() ,
+        'col': fields(Rooms)[1:]
     }
     return render(request, 'scheduler/home.html', context)
