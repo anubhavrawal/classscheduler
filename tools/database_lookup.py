30dd4be8297b446bbfbb0c7317675cece0103075
anubhavrawal <rawal.anubhav@outlook.com>
2021-10-04 19:13:25 -0400

    Imporoved db_loockup

--- tools/database_lookup.py
+++ tools/database_lookup.py
@@ -4,26 +4,32 @@ from pathlib import Path
 import sqlite3
 import sys
 
+def usage():
+    print("Usage: Python3 tools/database_lookup.py [-t] [-v] [tablename]")
+    print("[-t] : shows all the tables in the database")
+    print("[-v] [tablename]: shows few table for the given table")
+
 
 conn = sqlite3.connect("db.sqlite3")
 
 cursor = conn.cursor()
 
 mode = sys.argv[1]
+try:
+    if (mode ==  '-t' ):
+        tables = pd.read_sql_query("SELECT * from sqlite_master WHERE type='table' ", conn)
+        print(tables)
 
-if (mode ==  '-t' ):
-    tables = pd.read_sql_query("SELECT * from sqlite_master WHERE type='table' ", conn)
-    print(tables)
+    elif (mode== '-v'):
+        table =  sys.argv[2]
+        df3 = pd.read_sql('select * from '+ table, conn) #scheduler_rooms scheduler_meeting_times
+        print(df3.head())
 
-elif (mode== '-v'):
-    table =  sys.argv[2]
-    df3 = pd.read_sql('select * from '+ table, conn) #scheduler_rooms scheduler_meeting_times
-    print(df3.head())
-
-else:
-    print("Usage: Python3 tools/database_lookup.py [-t] [-v] [tablename]")
-    print("[-t] : shows all the tables in the database")
-    print("[-v] [tablename]: shows few table for the given table")
+    else:
+        usage()
+except Exception as e:
+    usage()
+    
 
 conn.close()
 
