aa23fe15328c4a09b9369510ddf045796c90c459
Mike17Mike <Mike17Mike@users.noreply.github.com>
2021-10-05 15:31:56 -0400

    Initial set up
    Working on each separate page

--- scheduler/views.py
+++ scheduler/views.py
@@ -4,36 +4,8 @@ from django.contrib.auth.models import User
 from scheduler.models import Instructors
 from scheduler.models import fields
 
-coloumns =[
-    'class',
-    'room',
-    'location',
-    'credits',
-    'hours'
-]
-
-
 data2 = list(Instructors.objects.values_list())
 colums_info = fields(Instructors)
-
-data = [
-    {
-        'class':"Computer Science 1",
-        'room':"C100",
-        'time':"1400",
-        'location':"Parkview",
-        'credits':"3",
-        'hours': "3"
-    },
-    {
-        'class':"Databases",
-        'room':"D208",
-        'time':"1100",
-        'location':"Parkview",
-        'credits':"4",
-        'hours': "4"
-    }
-]
 
 # Create your views here.
 def main_page(request):
