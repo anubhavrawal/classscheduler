864aab8f9e18aaafaf5670f538c6443042109cef
Brenden Rasmussen <54872409+brasmussen117@users.noreply.github.com>
2021-10-14 09:18:19 -0400

    Update urls.py

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.main_page, name='scheduler-home'),
-    path("scheduler/instructors/", views.instructors, name='scheduler-instructors'),
+    # path("scheduler/instructors/", views.instructors, name='scheduler-instructors'),
 ]
