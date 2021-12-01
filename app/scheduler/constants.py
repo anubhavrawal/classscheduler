f1afb2dcc7cd69f878d474ece5097da260cba3b2
Mike17Mike <Michael.L.Coffey@wmich.edu>
2021-12-01 13:51:20 -0500

    Fixed the parser
    Now grabs every single entry correctly

--- app/scheduler/constants.py
+++ app/scheduler/constants.py
@@ -1,3 +1,34 @@
 from .models import Semester
 
 semester_table = "scheduler_semester"
+
+### Setup of our array ###
+# 0 - CRN
+# 1 - course id
+# 2 - section
+# 3 - status
+# 4 - title
+# 5 - link1
+# 6 - link2
+# 7 - schedule type
+# 8 - reserved
+# 9 - credit hours
+# 10- billing hours
+# 11- contact hours
+# 12- Grad able
+# 13- cap
+# 14- waitlist cap
+# 15- special approval
+# 16- meeting type
+# 17- begin date
+# 18- end date
+# 19- location
+# 20- site
+# 21- primary_instructor
+# 22- fee
+# 23- comment
+# 24- meet_time
+# 25- dept
+# 26- semester year
+
+
